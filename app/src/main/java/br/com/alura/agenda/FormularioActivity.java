d93700e57c21ef632675f71a2406b83090cd4b27
Ruan Silva <ruanluiz.as@gmail.com>
2019-01-17 23:37:31 -0200

    aula 2

--- app/src/main/java/br/com/alura/agenda/FormularioActivity.java
+++ app/src/main/java/br/com/alura/agenda/FormularioActivity.java
@@ -9,6 +9,7 @@ import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -22,7 +23,11 @@ import java.io.File;
 
 import br.com.alura.agenda.dao.AlunoDAO;
 import br.com.alura.agenda.modelo.Aluno;
+import br.com.alura.agenda.retrofit.RetrofitInicializador;
 import br.com.alura.agenda.tasks.InsereAlunoTask;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class FormularioActivity extends AppCompatActivity {
 
@@ -90,7 +95,20 @@ public class FormularioActivity extends AppCompatActivity {
                 }
                 dao.close();
 
-                new InsereAlunoTask(aluno).execute();
+//                new InsereAlunoTask(aluno).execute();
+                Call call = new RetrofitInicializador().getAlunoService().insere(aluno);
+                call.enqueue(new Callback() {
+                    @Override
+                    public void onResponse(Call call, Response response) {
+                        Log.i("onResponse", "requisicao com sucesso");
+                    }
+
+                    @Override
+                    public void onFailure(Call call, Throwable t) {
+                        Log.e("onFailure", "requisicao falhou");
+                    }
+                }); //  Realiza requisição assíncrona, ou seja, cria uma thread separada
+                // da thread principal e executa em background.
 
                 Toast.makeText(FormularioActivity.this, "Aluno " + aluno.getNome() + " salvo!", Toast.LENGTH_SHORT).show();
 
---
+++ app/src/main/java/br/com/alura/agenda/retrofit/RetrofitInicializador.java
@@ -0,0 +1,21 @@
+package br.com.alura.agenda.retrofit;
+
+import br.com.alura.agenda.services.AlunoService;
+import retrofit2.Retrofit;
+import retrofit2.converter.jackson.JacksonConverterFactory;
+
+public class RetrofitInicializador {
+
+    private final Retrofit retrofit;
+
+    public RetrofitInicializador(){
+        this.retrofit = new Retrofit.Builder()
+                .baseUrl("http://192.168.1.34:8080/api/")
+                .addConverterFactory(JacksonConverterFactory.create())
+                .build();
+    }
+
+    public AlunoService getAlunoService() {
+        return retrofit.create(AlunoService.class);
+    }
+}
